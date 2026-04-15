f9364050ce8f32eab03d34173b2acf0e8eb600cf
iomiras <miras.shaltayev@gmail.com>
2026-04-15 13:06:38 +0500

    add deployment config for Render

--- federated-website/FastApi/main.py
+++ federated-website/FastApi/main.py
@@ -69,14 +69,20 @@ Frontend (CSV + config)
 # CORS - allow frontend dev servers
 # ---------------------------------------------------------------------------
 
+import os as _os
+
+_FRONTEND_URL = _os.environ.get("FRONTEND_URL", "")
+_allow_origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+]
+if _FRONTEND_URL:
+    _allow_origins.append(_FRONTEND_URL)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "*",
-    ],
+    allow_origins=_allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
